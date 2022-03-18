0c4def31d7c1dd38520c81f2d61b43f530540a94
obrunodev <brunorpdev@gmail.com>
2022-03-17 23:20:59 -0300

    :sparkles: Added sidebar with mdbootstrap

---
+++ base_static/js/scripts.js
@@ -0,0 +1,9 @@
+// SideNav Initialization
+$(".button-collapse").sideNav();
+
+var container = document.querySelector('.custom-scrollbar');
+var ps = new PerfectScrollbar(container, {
+  wheelSpeed: 2,
+  wheelPropagation: true,
+  minScrollbarLength: 20
+});
