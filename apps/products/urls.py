6309d42d1f3465c910539d40194e2ead7a345a19
Bruno R. Pianca <brunorpdev@gmail.com>
2022-03-25 12:48:42 -0300

    :sparkles: CRUD products finished

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,6 +5,7 @@ from .views import products
 app_name = 'products'
 urlpatterns = [
     path('', products.products_index, name='index'),
+    path('<int:product_id>/', products.product_detail, name='detail'),
     path('create/', products.products_create, name='create'),
     path('<int:product_id>/update/', products.products_update, name='update'),
     path('<int:product_id>/delete/', products.products_delete, name='delete'),
--- apps/products/views/products.py
+++ apps/products/views/products.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from products.forms import ProductForm
 from products.models import Product
@@ -7,12 +7,18 @@ from products.models import Product
 def products_index(request):
     context = {
         'form': ProductForm(),
+        'products': Product.objects.all(),
     }
     return render(request, 'products/pages/index.html', context)
 
 
-def product_detail(request):
-    ...
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'form': ProductForm(instance=product),
+        'product': product,
+    }
+    return render(request, 'products/pages/detail.html', context)
 
 
 def products_create(request):
@@ -26,8 +32,18 @@ def products_create(request):
 
 
 def products_update(request, product_id):
-    ...
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('products:index')
+    print(form.errors)
+    return redirect('products:index')
 
 
 def products_delete(request, product_id):
-    ...
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.delete()
+        return redirect('products:index')
+        
