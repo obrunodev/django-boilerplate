df488bcb3761dbc7d2e14fadb25203711b050839
Bruno R. Pianca <brunorpdev@gmail.com>
2022-03-24 21:02:54 -0300

    :sparkles: Started model creation

--- apps/crud_fbv/models.py
+++ apps/crud_fbv/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    price = models.FloatField()
+    quantity = models.IntegerField()
