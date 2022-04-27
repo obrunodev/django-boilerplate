fec05edf4ab3fb5efbe52f04caee2328f9a51330
obrunodev <brunorpdev@gmail.com>
2022-04-27 04:57:23 -0300

    :sparkles: feat: Confirm delete message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,28 +1,9 @@
-$(document).ready( function () {
+$(document).ready(() => {
 
-  $('#productTable').DataTable({
-    // "processing": true,
-    // "serverSide": true,
-    // "ajax": "/api/v1/accessories",
-    "stateSave": true,
-    "language": {
-      "lengthMenu": "Mostrar _MENU_ itens por página",
-      "zeroRecords": "Nenhum registro encontrado",
-      "info": "Mostrando _PAGE_ de _PAGES_",
-      "infoEmpty": "Nenhum registro encontrado",
-      "infoFiltered": "(Total de _MAX_ registros)",
-      "search": "Buscar:",
-      "paginate": {
-        "first":      "Primeiro",
-        "last":       "Último",
-        "next":       "Próximo",
-        "previous":   "Anterior"
-      },
+  $('#delete').click((e) => {
+    if (!confirm('Tem certeza que deseja deletar?')) {
+      e.preventDefault();
     }
-  });
-
-  $(document).on('click', '#deleteForm', () => {
-    return confirm("Tem certeza que deseja remover este item?");
   });
 
 });
