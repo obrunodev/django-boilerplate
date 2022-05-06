b802daeefb5107c6c52fa0187dee8e73841ea09d
Bruno R. Pianca <brunorpdev@gmail.com>
2022-05-06 04:23:17 -0300

    :sparkles: books CRUD done

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import books
 
-app_name = 'books'
 urlpatterns = [
-    path('', books.index, name='books_index')
+    path('', books.index, name='books_index'),
+    path('create/', books.create, name='books_create'),
+    path('update/<int:pk>/', books.update, name='books_update'),
+    path('delete/<int:pk>/', books.delete, name='books_delete'),
 ]
 
--- apps/books/views/books.py
+++ apps/books/views/books.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from books.models import Book
+from books.forms import BookForm
 
 
 def index(request):
@@ -9,12 +10,35 @@ def index(request):
 
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books_create')
+        print(form.errors)
+        return redirect('books_create')
+    else:
+        return render(request, 'books/pages/create.html')
 
 
-def update(request):
-    pass
+def update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('books_index')
+        return redirect('books_index')
+    else:
+        context = {'book': book}
+        return render(request, 'books/pages/update.html', context)
 
 
-def delete(request):
-    pass
+def delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('books_index')
+    else:
+        context = {'book': book}
+        return render(request, 'books/pages/delete.html', context)
