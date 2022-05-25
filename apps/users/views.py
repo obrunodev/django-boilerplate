d61eed5202ecd81a7331991cd3725b7f8a4b96d7
Bruno R. Pianca <brunorpdev@gmail.com>
2022-05-25 10:59:57 -0300

    First register form view

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm
+
 
 def register(request):
     if request.method == 'POST':
         pass
     else:
-        return render(request, 'users/pages/register.html')
+        context = {'form': RegisterForm()}
+        return render(request, 'users/pages/register.html', context)
 
 
 def login(request):
