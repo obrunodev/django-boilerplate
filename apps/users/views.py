623f675d971cb18958bc7c7c79324ba3ee6dad5e
Bruno R. Pianca <brunorpdev@gmail.com>
2022-05-26 15:27:42 -0300

    refactor: Register view (wip)

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 
 from .forms import RegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        pass
+        form = RegisterForm(request.POST or None)
+        return redirect('users:register')
     else:
         context = {'form': RegisterForm()}
         return render(request, 'users/pages/register.html', context)
