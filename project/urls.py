c76dbd18f1939b5797ddc5b225b36a5425dbbc3a
Bruno Pianca <bruno.pianca@alstratech.com>
2022-07-08 20:21:35 -0300

    fix: Adding media configs to URLs

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('crud_cbv/', include('crud_cbv.urls')),
     path('crud_fbv/', include('crud_fbv.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
